c4a50a3b1a95b66c5d9153455b6f328e697872e6
Tin Komericki <32905028+BigGuido55@users.noreply.github.com>
2019-05-11 03:57:24 -0700

    Added initial ROS package
    Added initial ROS package for calculating distance between a robot and an actor based on reference coordinate system (in this case, Gazebo's)

---
+++ robot_movement_control/tf_publish_actor.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from gazebo_msgs.srv import GetModelState
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+	rospy.init_node('animated_box')
+	rate = rospy.Rate(100)
+	while True:	
+		try:
+			broad = tf2_ros.TransformBroadcaster()
+			model = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+			coordinates = model('animated_box', '')
+
+			t = geometry_msgs.msg.TransformStamped()
+			t.header.stamp = rospy.Time.now()
+			t.header.frame_id = "ground_zero"
+			t.child_frame_id = "animated_box"
+			t.transform.translation.x = coordinates.pose.position.x
+			t.transform.translation.y = coordinates.pose.position.y
+			t.transform.translation.z = coordinates.pose.position.z
+			t.transform.rotation.x = coordinates.pose.orientation.x
+			t.transform.rotation.y = coordinates.pose.orientation.y
+			t.transform.rotation.z = coordinates.pose.orientation.z
+			t.transform.rotation.w = coordinates.pose.orientation.w
+
+			broad.sendTransform(t)
+
+		except rospy.ServiceException as e:
+			rospy.loginfo("Couldn't get model state: {0}".format(e))
+		rate.sleep()
+
+
