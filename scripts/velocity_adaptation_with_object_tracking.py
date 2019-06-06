a30b52ee13ca27f9e813d4a08a966573a07b2d2e
BigGuido55 <tin.komericki@fer.hr>
2019-06-06 04:39:24 -0700

    change in output

--- scripts/velocity_adaptation_with_object_tracking.py
+++ scripts/velocity_adaptation_with_object_tracking.py
@@ -14,6 +14,7 @@ class Node():
 		self.pioneerPose = data.pose
 
 	def write_robot_goal(self, data):
+                print "message recieved"
 		self.pioneerGoalPose = data.pose
 
 	def write_circles(self, data):
